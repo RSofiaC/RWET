84f5b4db1d43703b7f3e79aaf7cbdb276b3763fe
andySigler <andrewsigler1@gmail.com>
2017-03-02 13:19:36 -0500

    We are counting the times a word is reapiting in a text

---
+++ C_Mar3/wordcount.py
@@ -0,0 +1,22 @@
+import sys
+
+count = {}
+
+for line in sys.stdin:
+	line = line.strip()
+	words = line.split()
+	# how do I acutually count here?
+	for item in words:
+		if item in count:
+			count[item] += 1 #evaluate the value and store it into the key of the dictionary
+		else:
+			count[item] = 1
+
+
+#how do I make the output pretty?
+# you: 3
+# sort it by count descending? <- hard
+for key, val in count.iteritems():
+	print key + ":" + str(val)
+
+
