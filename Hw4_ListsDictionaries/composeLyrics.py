70a4459f4a584c0ab54e23977e7aaf0d6d7f57e5
Jacquelyn Zen <itpstudent@172-16-247-159.DYNAPOOL.NYU.EDU>
2017-03-01 23:12:44 -0500

    I tried to just mix biome and tropical and then substitute it with names of countries random words, but I actually like the lyrics I have

--- Hw4_ListsDictionaries/composeLyrics.py
+++ Hw4_ListsDictionaries/composeLyrics.py
@@ -1,11 +1,5 @@
 import random
 
-belt_lines = list()
-for line in open('newBelt.txt'):
-    line = line.strip()
-    if len(line) > 0:
-        belt_lines.append(line)
-
 biome_lines = list()
 for line in open('biome.txt'):
     line = line.strip()
@@ -19,7 +13,6 @@ for line in open('paisTropical.txt'):
         tropical_lines.append(line)
 
 for i in range(10):
-    random_belt = random.choice(belt_lines)
     random_biome = random.choice(biome_lines)
     random_tropical = random.choice(tropical_lines)
-    print random_belt[:len(random_belt)/2] + random_biome[len(random_biome)/2:]+ random_tropical[len(random_tropical)/2:]
+    print random_biome[:len(random_biome)/2:]+ random_tropical[len(random_tropical)/2:]
