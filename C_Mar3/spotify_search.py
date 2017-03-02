bd39e66855875fcdb0315d68aac05a99b911ad1f
andySigler <andrewsigler1@gmail.com>
2017-03-02 15:03:25 -0500

    we created a dictionary for the url, so we can modify the url with our query

--- C_Mar3/spotify_search.py
+++ C_Mar3/spotify_search.py
@@ -4,10 +4,13 @@ impor sys
 
 search_term = sys.argv[1]
 
-url = "http://api.spotify.com/v1/search?q=" +search_term + "&type=track"
+url = "http://api.spotify.com/v1/search?"
+params = urllib.urlencode({'q':search_term, 'limit':limit, 'type':'track'})
+url = base_url
+print url 
 
 raw = urllib.urlopen(url).read()
 data = json.loads(raw)
 
 for track in data["tracks"]["items"]:
-	print track ["name"]
+	print track ["name"], track["popularity"]
