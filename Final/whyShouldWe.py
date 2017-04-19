9c62e8a88e4d94e6ae7a9cdb389d6b57d483298c
itp student <itpstudent@172-16-193-46.DYNAPOOL.NYU.EDU>
2017-04-19 17:08:07 -0400

    Need to turn list items into strings so I can use in function

--- Final/whyShouldWe.py
+++ Final/whyShouldWe.py
@@ -1,3 +1,18 @@
 from __future__ import unicode_literals
 import spacy
-nlp 
+nlp = spacy.load('en')
+import random
+
+docR = nlp(open("peoplevsOhio.txt").read().decode('utf8'))
+docO = nlp(open("unwanted.txt").read().decode('utf8'))
+docQ = nlp(open("why.txt").read().decode('utf8'))
+
+questions = list(docQ.sents)
+offenses = list(docO.sents)
+requests = list(docR.sents)
+
+def understand (start, offense, question, request):
+        lineOne = 'Because '.join(random.choice(offense))
+        lineTwo = ', '.join(random.choice(question))
+        lineThree = random.choice(request)
+    return  start + " " + lineOne + " " + lineTwo + " " + lineThree
